9780ddf198a00fe30d2f946968a486b0a6005470
unknown <m.umarfarooq1175@gmail.com>
2025-06-14 22:26:59 +0530

    modifications

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -35,37 +35,3 @@ export const auth = getAuth(app);
 
 export const db = getFirestore(app);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // firebase.js
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-// const firebaseConfig = {
-
-//     apiKey: "AIzaSyBeJILrQVpcLAzlg2Kt1z5pt9aDe4OBxfM",
-//     authDomain: "auth-bd2d2.firebaseapp.com",
-//     projectId: "auth-bd2d2",
-//     storageBucket: "auth-bd2d2.firebasestorage.app",
-//     messagingSenderId: "350399634603",
-//     appId: "1:350399634603:web:45d8a88f56085952d3ebee",
-//     measurementId: "G-BQL71TBN56"
-// }
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-
-// export const db = getFirestore(app);
